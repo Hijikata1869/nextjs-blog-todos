3d286e6ad3b8574e8fa0a5f104f04249ab2ce290
Hijikata1869 <liv.knk@gmail.com>
2023-06-17 20:40:43 +0900

    Auth.js, blog-page.js, main-page.js, task-page.jsを作成

--- components/Auth.js
+++ components/Auth.js
@@ -41,18 +41,22 @@ export default function Auth() {
     if (isLogin) {
       login();
     } else {
-      await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/register`, {
-        method: "POST",
-        body: JSON.stringify({ username: username, password: password }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }).then((res) => {
-        if (res.status === 400) {
-          throw "authentication failed";
-        }
-      });
-      login();
+      try {
+        await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/register`, {
+          method: "POST",
+          body: JSON.stringify({ username: username, password: password }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }).then((res) => {
+          if (res.status === 400) {
+            throw "authentication failed";
+          }
+        });
+        login();
+      } catch (err) {
+        alert(err);
+      }
     }
   };
 
@@ -66,21 +70,25 @@ export default function Auth() {
           alt="Your Company"
         />
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
-          Sign in to your account
+          {isLogin ? "Login" : "Sign up"}
         </h2>
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST">
+        <form className="space-y-6" onSubmit={authUser}>
           <div>
             <div className="mt-2">
               <input
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
+                name="username"
+                type="text"
+                autoComplete="username"
                 required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                }}
               />
             </div>
           </div>
@@ -88,19 +96,26 @@ export default function Auth() {
           <div>
             <div className="flex items-center justify-center">
               <div className="text-sm">
-                <span className="cursor-pointer font-semibold text-white hover:text-indigo-500">
+                <span
+                  onClick={() => setIsLogin(!isLogin)}
+                  className="cursor-pointer font-semibold text-white hover:text-indigo-500"
+                >
                   Change Mode ?
                 </span>
               </div>
             </div>
             <div className="mt-2">
               <input
-                id="password"
                 name="password"
                 type="password"
                 autoComplete="current-password"
                 required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="password"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
               />
             </div>
           </div>
@@ -110,7 +125,7 @@ export default function Auth() {
               type="submit"
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Sign in
+              {isLogin ? "Login with JWT" : "Create new user"}
             </button>
           </div>
         </form>
